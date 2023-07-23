cd551ef9667ba81b785bddd6146cc3b0b721229e
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-07-23 18:23:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# deutsch-c1-quiz-drei
+
+Ein Quiz auf dem Niveau C1. Inhalt der 30 Aufgaben sind Grammatik, Wortschatz und Strukturen.
---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Die Reaktion ihrer Freunde war unterschiedlich?",
+        answersList: [
+            {answer:"Ja, ihre Freunde reagierten verschieden.", isCorrect:true},
+            {answer:"Richtig, ihre Freunde reagieren alle gleich.", isCorrect:false},
+            {answer:"Genau, ihre Freunde haben richtig reagiert.", isCorrect:false},
+        ]
+    },{
+        question:"Das Team analysierte das Problem und fand eine Lösung.",
+        answersList: [
+            {answer:"Die Analyse brachte ein Problem für das Team.", isCorrect:false},
+            {answer:"Die Analyse des Problems führte zu einer Lösung.", isCorrect:true},
+            {answer:"Das Problem der Analyse beschäftigt das Team.", isCorrect:false},
+        ]
+    },{
+        question:"Wir gaben der Kellnerin ein großzügiges Trinkgeld.",
+        answersList: [
+            {answer:"Die Kellnerin erwartete ein üppiges Trinkgeld.", isCorrect:false},
+            {answer:"Die Freude über das großzügige Trinkgeld ärgerte die Kellnerin.", isCorrect:false},
+            {answer:"Die großzügige Gabe von Trinkgeld freute die Kellnerin sehr.", isCorrect:true},
+        ]
+    },{
+        question:"Nach der Ankunft suchten wir eine passende Unterkunft.",
+        answersList: [
+            {answer:"Die Suche nach einem Hotel war sehr anstrengend.", isCorrect:true},
+            {answer:"Die Suche einer Unterkunft war nicht mehr nötig.", isCorrect:false},
+            {answer:"Die beste Suche gelingt mithilfe von Buchungsportalen.", isCorrect:false},
+        ]
+    },{
+        question:"Manche Versicherungen heben wahrscheinlich die Tarife an.",
+        answersList: [
+            {answer:"Mit dem Anheben der Tarife ist nicht mehr zu rechnen.", isCorrect:false},
+            {answer:"Eine mögliche Verteuerung der Versicherungen ist zu erwarten. ", isCorrect:true},
+            {answer:"Die Tarife der Versicherer sind wahrscheinlich sehr hoch.", isCorrect:false},
+        ]
+    },{
+        question:"Herr Winter hat sich verschuldet, ",
+        answersList: [
+            {answer:"zumal seine Gewinne umfangreich waren.", isCorrect:false},
+            {answer:"denn seine Schulden wollte er nicht mehr bezahlen.", isCorrect:false},
+            {answer:"weil seine Verluste immer größer wurden.", isCorrect:true},
+        ]
+    },{
+        question:"Freizeit war ihm nicht so wichtig,",
+        answersList: [
+            {answer:"zumal er am Wochenende frei hatte.", isCorrect:true},
+            {answer:"denn er hatte jedes Wochenende frei.", isCorrect:false},
+            {answer:"obwohl er keine freien Wochenenden hatte.", isCorrect:false},
+        ]
+    },{
+        question:"Viele Anleger in Kryptowährungen haben Geldprobleme.",
+        answersList: [
+            {answer:"Denn woher sollen sie das Risiko kennen?", isCorrect:false},
+            {answer:"Sie wissen eben nicht, wie riskant solche Anlagen sind.", isCorrect:true},
+            {answer:"Sie haben nämlich gute Kenntnisse in Anlagewerten.", isCorrect:false},
+        ]
+    },{
+        question:"Mangels Erfahrung in Wertanlagen",
+        answersList: [
+            {answer:"haben manche enorme Verluste erwartet.", isCorrect:false},
+            {answer:"haben sehr viele große Gewinne erhofft.", isCorrect:false},
+            {answer:"haben sich viele zu stark verschuldet.", isCorrect:true},
+        ]
+    },{
+        question:"Dank neuer wissenschaftlicher Erkenntnisse",
+        answersList: [
+            {answer:"kann man bald mit einer Verbesserung rechnen.", isCorrect:true},
+            {answer:"wird die Rechnung bald besser aufgehen.", isCorrect:false},
+            {answer:"sollten wir keine Verbesserung erwarten.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Eine dringend zu lösende Aufgabe ist eine,",
+        answersList: [
+            {answer:"die so schnell wie möglich gelöst werden muss.", isCorrect:true},
+            {answer:"die nicht unbedingt gelöst werden kann.", isCorrect:false},
+            {answer:"die unter Umständen schwierig zu lösen ist.", isCorrect:false},
+        ]
+    },{
+        question:"Eine einfach durchzuführende Prüfung ist eine, die",
+        answersList: [
+            {answer:"nicht problemlos organisiert werden kann.", isCorrect:false},
+            {answer:"nicht besonderer Vorbereitungen bedarf.", isCorrect:true},
+            {answer:"nicht ohne Hilfe zu machen ist.", isCorrect:false},
+        ]
+    },{
+        question:"Den Patienten wird anschaulich dargelegt,",
+        answersList: [
+            {answer:"wie sie therapiert werden sollen.", isCorrect:false},
+            {answer:"warum sie die Therapie vermeiden können.", isCorrect:false},
+            {answer:"wie die Therapie durchgeführt wird.", isCorrect:true},
+        ]
+    },{
+        question:"Die Ratsuchenden bekommen ausführlich erklärt",
+        answersList: [
+            {answer:"an welche Hilfsorganisationen sie sich wenden können.", isCorrect:true},
+            {answer:"bei wem sie keine Hilfe bekommen werden.", isCorrect:false},
+            {answer:"welche Hilfeleistungen nicht zu erwarten sind.", isCorrect:false},
+        ]
+    },{
+        question:"Eine rationale Entscheidung",
+        answersList: [
+            {answer:"kann unsere Überlegungen schneller umsetzen.", isCorrect:false},
+            {answer:"beruht auf vernunftgesteuerten Überlegungen.", isCorrect:true},
+            {answer:"sollte sich auf subjektive Überzeugungen stützen.", isCorrect:false},
+        ]
+    },{
+        question:"Dieses Verhalten von Jugendlichen ist nicht tolerabel.",
+        answersList: [
+            {answer:"Ja eben! Es kann gut ausgehalten werden.", isCorrect:false},
+            {answer:"Genau! Denn unsere Toleranz kennt keine Grenzen.", isCorrect:false},
+            {answer:"Richtig! Wir sollten das nicht zulassen.", isCorrect:true},
+        ]
+    },{
+        question:"Die Kinder sollen dazu gebracht werden ihre Fehler einzusehen.",
+        answersList: [
+            {answer:"Wir wollen, dass Kinder aus ihren Fehlern lernen.", isCorrect:true},
+            {answer:"Die Kinder sollen lernen, Fehler zu machen.", isCorrect:false},
+            {answer:"Die Lehrer wollen nicht, dass Kinder Fehler begehen.", isCorrect:false},
+        ]
+    },{
+        question:"Der Ratgeber soll bei der Entscheidung helfen.",
+        answersList: [
+            {answer:"Der Verlag möchte, dass wir die Entscheidung treffen.", isCorrect:false},
+            {answer:"Die Autoren wollen Hilfe bei Entscheidungen leisten.", isCorrect:true},
+            {answer:"Ohne seine Hilfe können wir nichts entscheiden.", isCorrect:false},
+        ]
+    },{
+        question:"Welches Thema behandelt die Grafik",
+        answersList: [
+            {answer:"und wie gefallen ihnen die Balkendiagramme?", isCorrect:false},
+            {answer:"und in welcher Reihenfolge gehen Sie darauf ein?", isCorrect:false},
+            {answer:"und welche wichtigen Aussagen entnehmen Sie ihr?", isCorrect:true},
+        ]
+    },{
+        question:"Das vorliegende Schaubild",
+        answersList: [
+            {answer:"behandelt das Thema anschaulich.", isCorrect:true},
+            {answer:"verhandelt mit uns über das Thema.", isCorrect:false},
+            {answer: "veranschaulicht meine Zusammenfassung.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Die Grafik veranschaulicht das Ergebnis einer Umfrage.",
+        answersList: [
+            {answer:"Sie zeigt uns die Antworten auf bestimmte Fragen.", isCorrect:true},
+            {answer:"Das Resultat unserer Fragen ist die Umfrage.", isCorrect:false},
+            {answer:"Das Diagramm haben wir bei den Fragen benutzt.", isCorrect:false},
+        ]
+    },{
+        question:"Im Herbst werden wir die Anlage wieder in Betrieb setzen.",
+        answersList: [
+            {answer:"Unter der Bedingung, dass der Betrieb erfolgt.", isCorrect:false},
+            {answer:"Vorausgesetzt, dass wir die Reparatur beenden.", isCorrect:true},
+            {answer:"Falls die Reparatur länger dauern sollte.", isCorrect:false},
+        ]
+    },{
+        question:"Sie sollten möglichst keine Schadstoffe produzieren.",
+        answersList: [
+            {answer:"Andererseits können Sie nicht weiter expandieren.", isCorrect:false},
+            {answer:"Sonst kann die Produktion erweitert werden.", isCorrect:false},
+            {answer:"Andernfalls werden die Umweltprobleme immer größer.", isCorrect:true},
+        ]
+    },{
+        question:"Leider haben die ersten Versuche keinen Erfolg gezeigt.",
+        answersList: [
+            {answer:"Ungeachtet der Tatsache, dass wir über die modernste Technik verfügen.", isCorrect:true},
+            {answer:"Obwohl wir überwiegend unbewährte Methoden gewählt haben.", isCorrect:false},
+            {answer:"Trotz der veralteten technischen Ausstattung unserer Labore.", isCorrect:false},
+        ]
+    },{
+        question:"Ungeachtet der besonderen klimatischen Bedingungen",
+        answersList: [
+            {answer:"wäre an Reisende in diesem Land nicht zu denken.", isCorrect:false},
+            {answer:"ist auf die generelle Situation keine Rücksicht zu nehmen.", isCorrect:false},
+            {answer:"konnten wir die Exkursion erfolgreich durchführen.", isCorrect:true},
+        ]
+    },{
+        question:"Im Falle einer unvorhergesehenen Verschlechterung",
+        answersList: [
+            {answer:"sollten Sie umgehend einen Arzt aufsuchen.", isCorrect:true},
+            {answer:"können Sie mit Verbesserungen rechnen.", isCorrect:false},
+            {answer:"müssen Sie frühzeitig die Apotheke informieren.", isCorrect:false},
+        ]
+    },{
+        question:"Wir sind stets Ihren Tipps gegenüber aufgeschlossen.",
+        answersList: [
+            {answer:"Wir reservieren uns das Recht Sie auszuschließen.", isCorrect:false},
+            {answer:"Wir freuen uns jederzeit sehr über Ihre Empfehlungen.", isCorrect:true},
+            {answer:"Ihre Tipps könnten unsere Unternehmensstrategie gefährden.", isCorrect:false},
+        ]
+    },{
+        question:"Ihre Argumentation hat mich voll überzeugt.",
+        answersList: [
+            {answer:"Allerdings sind mir manche Punkte noch unklar.", isCorrect:false},
+            {answer:"Außerdem sind manche Gründe fragwürdig.", isCorrect:false},
+            {answer:"Ich kann Ihre Begründung gut nachvollziehen.", isCorrect:true},
+        ]
+    },{
+        question:"Die Wissenschaft sucht nach Möglichkeiten der Verbesserung.",
+        answersList: [
+            {answer:"Andernfalls gibt es keine Aussicht auf Lösungen.", isCorrect:true},
+            {answer:"Obwohl sie alternative Lösungen nicht unterstützt.", isCorrect:false},
+            {answer:"Denn darin liegt nicht ihre öffentliche Bedeutung.", isCorrect:false},
+        ]
+    },{
+        question:"Bist du schon gespannt auf das nächste Treffen?",
+        answersList: [
+            {answer:"Ja klar. Die Stimmung war sehr angespannt.", isCorrect:false},
+            {answer:"Na ja. Die nächste Show wird sicher spannend.", isCorrect:false},
+            {answer:"Ich kann es kaum bis zum nächsten Mal erwarten.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
